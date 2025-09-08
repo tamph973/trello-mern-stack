ddef3e9bfaedcc7724a465af62f6060827c5d74b
tamph973 <phtam0907@gmail.com>
2025-09-09 02:41:33 +0700

    Add theme mode selection and update theme configuration

--- trello-web/src/theme.js
+++ trello-web/src/theme.js
@@ -1,22 +1,25 @@
 import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { red, teal } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = createTheme({
-	cssVariables: true,
-	palette: {
-		mode: 'light',
-		primary: {
-			main: red[500]
+	cssVariables: {
+		colorSchemeSelector: 'class'
+	},
+	colorSchemes: {
+		dark: {
+			palette: {
+				primary: {
+					main: red[500]
+				}
+			}
 		},
-		secondary: {
-			main: '#19857b'
-		},
-		error: {
-			main: red.A400
-		},
-		text: {
-			secondary: red[500]
+		light: {
+			palette: {
+				primary: {
+					main: teal[500]
+				}
+			}
 		}
 	}
 })
